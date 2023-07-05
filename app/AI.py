325e7b5df30579ef4377b55b28c0c4136fa71aa3
Felipe Buritica <andres.ruizburitica@gmail.com>
2023-07-04 22:56:16 -0500

    Add comments and docstrings

--- app/AI.py
+++ app/AI.py
@@ -1,69 +1,181 @@
 import chess
 import math
-from utils import PIECE_VALUES, POSITION_VALUES
-def alphabeta_pruning(boardCopy,movement,depth,alpha,beta,maximizingPlayer):
-    if depth == 0:
-        return evaluateBoard(boardCopy,movement)
-    
-    boardCopy.push(chess.Move.from_uci(movement))
-    legal_moves = [str(mov) for mov in boardCopy.legal_moves]
 
-    if maximizingPlayer:
+from utils import PIECE_VALUES, POSITION_VALUES
+
+
+
+def alphabeta_pruning(board, movement, depth, alpha, beta, maximizing_player):
+    """
+    Implementa la poda alpha-beta.
+
+        board : estado actual del tablero.
+        movement : movimiento actual.
+        depth : profundidad actual del árbol de búsqueda.
+        alpha : valor alfa.
+        beta : valor beta.
+        maximizing_player : indicador que especifica si el jugador actual está
+                            maximizando o minimizando.
+    """
+    # Verificamos si hemos alcanzado la profundidad máxima de búsqueda.
+    if depth == 0:
+        return evaluate_board(board, movement)
+
+    # Aplicamos el movimiento actual al tablero utilizado.
+    board.push(chess.Move.from_uci(movement))
+
+    # Obtenemos todos los movimientos legales disponibles para el estado actual del tablero.
+    legal_moves = [str(mov) for mov in board.legal_moves]
+
+    # Si el jugador en el nivel actual del árbol es el jugador maximizador,
+    # se realiza una búsqueda maximizadora.
+    if maximizing_player:
+        # Inicializamos value como -Infinite.
         value = -(math.inf)
+
+        # Para cada movimiento se reliza una llamada recursiva de alphabeta_pruning()
+        # con una profundidad reducida de 1 y se invierte el valor de maximizing_player.
         for move in legal_moves:
-            value = max(value,alphabeta_pruning(boardCopy.copy(),move,depth-1,alpha,beta,False))
+            value = max(value, alphabeta_pruning(board.copy(), move, depth-1, alpha, beta, False))
+
+            # Si value es mayor o igual a beta, se realiza el corte beta y se sale del bucle,
+            # ya que se ha encontrado un valor que el jugador minimizador no permitiría.
             if value >= beta:
                 break
-            alpha = max(alpha,value)
+
+            alpha = max(alpha, value)
+
         return value
+
+    # Si no, se realiza una búsqueda minimizadora.
     else:
+        # Inicializamos value como +Infinite.
         value = (math.inf)
+
         for move in legal_moves:
-            value = min(value,alphabeta_pruning(boardCopy.copy(),move,depth-1,alpha,beta,True))
+            value = min(value, alphabeta_pruning(board.copy(), move, depth-1, alpha, beta, True))
+
+            # Si value es menor o igual que alpha se realiza el corte alpha y se sale del bucle.
             if value <= alpha:
                 break
-            beta = min(beta,value)
+
+            beta = min(beta, value)
+
         return value
 
-def evaluateBoard(boardCopy,movement):
+
+
+def evaluate_board(board, movement):
+    """
+    Evalúa el estado del tablero en función de los valores asignados a las
+    piezas y las posiciones.
+
+        board : estado del tablero.
+        movement : movimiento actual.
+    """
+    # Creamos un acumulador para la puntuación de la evaluación.
     value = 0
-    boardCopy.push(chess.Move.from_uci(movement))
+
+    # Aplicamos el movimiento actual al tablero.
+    board.push(chess.Move.from_uci(movement))
+
+    # Recorremos todas las casillas del tablero.
     for i in range(8):
         for j in range(8):
-            piece = str(boardCopy.piece_at(chess.Square((i*8+j))))
-            pieceVal = PIECE_VALUES[piece] if piece != 'None' else 0
-            posVal = POSITION_VALUES[piece][i][j] if piece != 'None' else 0
-            value += pieceVal + posVal
+            # Para cada casilla obtenemos la pieza en esa posición.
+            piece = str(board.piece_at(chess.Square((i * 8 + j))))
+
+            # Obtenemos el valor asignado a la pieza.
+            # Si no hay pieza en esa casilla, se asigna un valor de 0.
+            piece_val = PIECE_VALUES[piece] if piece != 'None' else 0
+
+            # Obtenemos el valor asignado a la posición de la pieza.
+            pos_val = POSITION_VALUES[piece][i][j] if piece != 'None' else 0
+
+            value += piece_val + pos_val
+
     return value
 
-def minMaxMax(boardCopy,movement,depth):
+
+
+def min_max_max(board, movement, depth):
+    """
+    Implementa el algoritmo Minimax para la búsqueda del mejor movimiento.
+    Esta función busca el movimiento que maximiza el valor de evaluación en
+    un nivel determinado del árbol de búsqueda.
+
+        board : estado del tablero.
+        movement : movimiento actual.
+        depth : profundidad actual de la búsqueda.
+    """
+    # Si la profundidad es menor a 0, se alcanzó el nivel máximo de profundidad.
     if depth < 0:
-        value = evaluateBoard(boardCopy,movement)
-        return {"Value":value,"Movement":movement}
-    
-    boardCopy.push(chess.Move.from_uci(movement))
-    max = -(math.inf)
-    legal_moves = [str(mov) for mov in boardCopy.legal_moves]
+        value = evaluate_board(board, movement)
+
+        return {
+            "value": value,
+            "movement": movement
+        }
+
+    # Aplicamos el movimiento al tablero actual.
+    board.push(chess.Move.from_uci(movement))
+
+    # Inicializamos la variable como -Infinite.
+    maximum = -(math.inf)
+
+    # Obtenemos todas las jugadas legales disponibles en el tablero.
+    legal_moves = [str(mov) for mov in board.legal_moves]
+
     result = {}
+
+    # Para cada movimiento legal obtenemos aquel con la mayor puntuación.
     for move in legal_moves:
-       evaluation = minMaxMin(boardCopy.copy(),move,depth-1)
-       if  evaluation["Value"] > max:
-            max = evaluation["Value"]
+       evaluation = min_max_min(board.copy(), move, depth-1)
+
+       if  evaluation["value"] > maximum:
+            maximum = evaluation["value"]
             result = evaluation
+
     return result
 
-def minMaxMin(boardCopy,movement,depth):
+
+
+def min_max_min(board, movement, depth):
+    """
+    Implementa el algoritmo Minimax para la búsqueda del mejor movimiento.
+    Esta función busca el movimiento que minimiza el valor de evaluación en
+    un nivel determinado del árbol de búsqueda.
+
+        board : estado del tablero.
+        movement : movimiento actual.
+        depth : profundidad actual de la búsqueda.
+    """
+    # Si la profundidad es menor a 0, se alcanzó el nivel máximo de profundidad.
     if depth < 0:
-        value = evaluateBoard(boardCopy,movement)
-        return {"Value":value,"Movement":movement}
-    
-    boardCopy.push(chess.Move.from_uci(movement))
-    min = math.inf
-    legal_moves = [str(mov) for mov in boardCopy.legal_moves]
+        value = evaluate_board(board, movement)
+
+        return {
+            "value": value,
+            "movement": movement
+        }
+
+    # Aplicamos el movimiento al tablero actual.
+    board.push(chess.Move.from_uci(movement))
+
+    # Inicializamos la variable como +Infinite.
+    minimum = math.inf
+
+    # Obtenemos todas las jugadas legales disponibles en el tablero.
+    legal_moves = [str(mov) for mov in board.legal_moves]
+
     result = {}
+
+    # Para cada movimiento legal obtenemos aquel con la menor puntuación.
     for move in legal_moves:
-       evaluation = minMaxMax(boardCopy.copy(),move,depth-1)
-       if  evaluation["Value"] < min:
-            min = evaluation["Value"]
+       evaluation = min_max_max(board.copy(), move, depth-1)
+
+       if  evaluation["Value"] < minimum:
+            minimum = evaluation["Value"]
             result = evaluation
+
     return result
--- app/main.py
+++ app/main.py
@@ -2,138 +2,290 @@ import pygame
 import sys
 import chess
 import math
-from tkinter import messagebox
-
-from utils import PIECE_IMAGES
 
 from AI import alphabeta_pruning
+from utils import PIECE_IMAGES
 
+
+
+# Inicializamos el tablero de ajedrez.
 board = chess.Board()
 
+# Definimos el ancho de la ventana.
 WIDTH = 650
 
+# Creamos una ventana con el ancho y alto definidos.
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
+
+# Establecemos el título de la ventana del programa.
 pygame.display.set_caption("Crazy Chess")
+
+# Establecemos los colores RGB
 WHITE = (238, 238, 211)
 GREEN = (118, 150, 86)
 YELLOW = (204, 204, 0)
 BLUE = (50, 255, 255)
 BLACK = (0, 0, 0)
 
+
+
 class Node:
+    """
+    Representación de una celda del tablero de ajedrez.
+    """
+
     def __init__(self, row, col, width):
+        """
+        Inicializa un nodo.
+
+            row : fila en la que se ubica el nodo.
+            col : columna en la que se ubica el nodo.
+            width : ancho del nodo.
+        """
         self.row = row
         self.col = col
         self.x = int(col * width)
         self.y = int(row * width)
         self.colour = WHITE
 
-    def draw(self, WIN):
-        pygame.draw.rect(WIN, self.colour, (self.x, self.y, WIDTH / 8, WIDTH / 8))
 
-    def setup(self, WIN, boardM):
-        if boardM[self.row][self.col] != "None":
-            image = pygame.image.load(PIECE_IMAGES[boardM[self.row][self.col]])
+    def draw(self, window):
+        """
+        Dibuja la celda en la ventana de visualización.
+
+            window : ventana de visualización.
+        """
+        pygame.draw.rect(window, self.colour, (self.x, self.y, WIDTH / 8, WIDTH / 8))
+
+
+    def setup(self, window, matrix):
+        """
+        Establece las fichas de ajedrez en el tablero de juego.
+
+            window: ventana de visualización.
+            matrix : matriz que representa el tablero de ajedrez.
+        """
+        # Si la celda en la posición dada no está vacía, crea la imagen, la escala
+        # y la ubica en la posición correspondiente.
+        if matrix[self.row][self.col] != "None":
+            image = pygame.image.load(PIECE_IMAGES[matrix[self.row][self.col]])
             scaled_image = pygame.transform.scale(image, (int(image.get_width() / 1.8), int(image.get_height() / 1.8)))
-            WIN.blit(scaled_image, (self.x, self.y))
+            window.blit(scaled_image, (self.x, self.y))
 
 
-def make_grid(rows, width):
+
+def make_grid(size, width):
+    """
+    Devuelve una cuadrícula de nodos.
+
+        size : representa el número de filas y columnas del tablero.
+        width : indica el ancho total de la cuadrícula.
+    """
     grid = []
-    gap = width // rows
-    print(gap)
-    for i in range(rows):
+
+    # Calculamos el ancho de cada casilla.
+    gap = width // size
+
+    # Añadimos cada nodo a la cuadrícula.
+    for i in range(size):
         grid.append([])
-        for j in range(rows):
+
+        for j in range(size):
             node = Node(i, j, gap)
+
             grid[i].append(node)
-            if (i+j)%2 ==1:
+
+            # si i + j es un número impar, se pinta de verde la celda.
+            if (i + j) % 2 == 1:
                 grid[i][j].colour = GREEN
+
     return grid
 
-def draw_grid(win, rows, width):
+
+
+def draw_grid(window, size, width):
+    """
+    Dibuja las líneas de la cuadrícula en la ventana del juego.
+
+        window : ventana de visualización.
+        size : número de filas y columnas de la cuadrícula.
+        width : ancho total de la cuadrícula.
+    """
+    # Calculamos el ancho de cada celda.
     gap = width // 8
-    for i in range(rows):
-        pygame.draw.line(win, BLACK, (0, i * gap), (width, i * gap))
-        for j in range(rows):
-            pygame.draw.line(win, BLACK, (j * gap, 0), (j * gap, width))
 
-def update_display(win, grid, rows, width):
-    boardM = []
+    # Pintamos las líneas de color negro.
+    for i in range(size):
+        pygame.draw.line(window, BLACK, (0, i * gap), (width, i * gap))
+        for j in range(size):
+            pygame.draw.line(window, BLACK, (j * gap, 0), (j * gap, width))
+
+
+
+def update_display(window, grid, rows, width):
+    """
+    Actualiza la ventana del tablero de ajedrez.
+
+        window : ventana de visualización.
+        grid : cuadrícula que representa el tablero de ajedrez.
+        rows : número de filas del tablero.
+        width : ancho total de la ventana de juego.
+    """
+    matrix = []
+
+    # Recorremos el tablero y guardamos cada tipo de pieza en la matriz.
     for i in range(8):
-        arr = [str(board.piece_at(chess.Square((i*8+j)))) for j in range(8)]
-        boardM.insert(0,arr)
+        arr = [str(board.piece_at(chess.Square((i * 8 + j)))) for j in range(8)]
+        matrix.insert(0, arr)
 
+    # Recorremos de nuevo la cuadrícula y dibujamos cada nodo.
     for row in grid:
         for spot in row:
-            spot.draw(win)
-            spot.setup(win,boardM)
-    draw_grid(win, rows, width)
+            spot.draw(window)
+            spot.setup(window, matrix)
+
+    # Dibujamos las líneas de la cuadrícula.
+    draw_grid(window, rows, width)
+
+    # Actualizamos la ventana de juego para mostrar los cambios hechos.
     pygame.display.update()
 
-def Find_Node(pos, WIDTH):
-    interval = WIDTH / 8
+
+
+def find_node(pos, width):
+    """
+    Encuentra la posición del nodo en el tablero de ajedrez a partir de una posición
+    dada en píxeles en la ventana del juego.
+
+        pos : tupla que representa la posición en pixeles (x, y) dentro de la ventana.
+        width : ancho total de la ventana de juego.
+    """
+    # Calculamos el ancho de cada nodo.
+    interval = width / 8
+
+    # Definimos la fila y columna del nodo.
     y, x = pos
-    rows = y // interval
-    columns = x // interval
-    x,y = int(rows), int(columns)
-    pos = "" + ['a','b','c','d','e','f','g','h'][x] + f"{8-y}"
+    row = y // interval
+    col = x // interval
+
+    x, y = int(row), int(col)
+
+    # Obtenemos la posicíon del nodo en la notación del ajedrez.
+    pos = "" + ['a','b','c','d','e','f','g','h'][x] + f"{8 - y}"
+
     return pos
 
-def machine_move(boardCopy):
-    max = -(math.inf)
+
+
+def machine_move(board):
+    """
+    Realiza el movimiento por parte de la máquina.
+
+        board : tablero de ajedrez.
+    """
+    maximum = -(math.inf)
     movement = ""
-    legal_moves = [str(mov) for mov in boardCopy.legal_moves]
+
+    # Obtenemos todos los movimientos legales disponibles en el tablero.
+    legal_moves = [str(mov) for mov in board.legal_moves]
+
+    # Para cada movimiento legal se realiza la poda alpha-beta con una profundidad
+    # máxima de 3 y un indicador False para señalar que es el turno de la máquina.
     for move in legal_moves:
-        result = alphabeta_pruning(boardCopy.copy(),move,3,-(math.inf),math.inf,False)
-        if result > max:
+        result = alphabeta_pruning(board.copy(), move, 3, -(math.inf), math.inf, False)
+
+        # Se busca que el movimiento tenga el máximo valor.
+        if result > maximum:
             movement = move
-            max = result   
+            maximum = result
+
     return movement
 
-def main(WIN, WIDTH):
+
+
+def main(window, width):
+    """
+    Función principal que ejecuta el juego de ajedrez.
+
+        window : ventana de visualización.
+        width : ancho total de la ventana.
+    """
+    # Creamos una variable para almacenar el movimiento del jugador.
     movement = ""
-    grid = make_grid(8, WIDTH)
+
+    # Creamos la cuadrícula del tablero de ajedrez.
+    grid = make_grid(8, width)
+
     while True:
-        #pygame.time.delay(10)
         for event in pygame.event.get():
+
+            # Si se ejecuta un evento QUIT, se cierra el programa.
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            # Establecemos el título de la ventana de juego.
             pygame.display.set_caption("Crazy Chess | Your turn")
+
+            # Si se presiona el mouse.
             if event.type == pygame.MOUSEBUTTONDOWN:
-                node = Find_Node(pygame.mouse.get_pos(), WIDTH)
+                # Obtenemos el nodo que ha sido seleccionado.
+                node = find_node(pygame.mouse.get_pos(), width)
+
+                # Si movement es una cadena vacía, significa que el jugador aún
+                # no ha seleccionado ninguna pieza para mover.
                 if movement == "":
+                    # Obtenemos la pieza seleccionada.
                     piece = board.piece_at(chess.Square(chess.parse_square(node)))
-                    if piece != None and str(piece).isupper(): 
+
+                    # Si la pieza es válida, la asignamos al movimiento.
+                    if piece != None and str(piece).isupper():
                         movement = node
                     else:
                         print("Not Valid")
+
+                # Si movement no es una cadena vacía y es igual al nodo seleccionado,
+                # significa que el jugador ha hecho clic nuevamente en la misma pieza
+                # para cancelar la selección.
                 elif movement == node:
                     movement = ""
+
+                # Si movement no es una cadena vacía y es diferente del nodo seleccionado,
+                # significa que el jugador ha seleccionado un nodo de destino para su movimiento.
                 else:
+                    # Agregamos el nodo al final de la cadena movement.
                     movement += node
+
+                    # Realizamos el movimiento.
                     move = chess.Move.from_uci(movement)
-                    if not move in board.legal_moves:
+
+                    # Verificamos si el movimiento no es válido.
+                    if move not in board.legal_moves:
                         print("Not valid move")
                         movement = ""
+
+                    # Si es válido, realizamos el movimiento y actualizamos el tablero.
                     else:
                         if board.is_capture(move):
                             print('CAPTURA')
 
                         board.push(chess.Move.from_uci(movement))
                         movement = ""
-                        
-                        update_display(WIN, grid, 8, WIDTH)
+                        update_display(window, grid, 8, width)
 
-                        #IA
+                        # Después del turno del jugador, indicamos que es turno de la IA.
                         pygame.display.set_caption("Crazy Chess | IA turn")
+
+                        # La máquina selecciona el movimiento a hacer.
                         movement = machine_move(board.copy())
+
+                        # Realiza el movimiento.
                         board.push(chess.Move.from_uci(movement))
                         movement = ""
 
-            update_display(WIN, grid, 8, WIDTH)
+            update_display(window, grid, 8, width)
 
 
 
+# Ejecutamos el juego.
 main(WIN, WIDTH)
